f3f3a5163517cf00b0e0d58eac45367b422adf01
kevinjyx <kevinjyx@gmail.com>
2020-10-09 15:56:46 -0400

    flappy bird

---
+++ Assets/Scripts/bird.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    private const float JUMP_AMOUNT = 100f;
+    private Rigidbody birdrigidbody;
+    private void Awake()
+    {
+       birdrigidbody = GetComponent<Rigidbody>();
+    }
+
+        private void update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+ 
+    private void Jump()
+    {
+        birdrigidbody.velocity= Vector2.up * JUMP_AMOUNT;
+    }
+
+}
